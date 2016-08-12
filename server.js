51d11cd915b1d0633e15fd8fc421528ca441a182
Emad Kheiralla <emadkheiralla@Emads-MBP.home>
2016-08-12 01:22:47 -0400

    Initialized connection.js orm.js burger.js and server.js files

--- server.js
+++ server.js
@@ -1,3 +1,50 @@
 var express = require('express');
-var express = require('method-override');
-var express = require('body-parser');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var myConnection = require('./config/connection.js');
+
+var app = express();
+app.use('/static', express.static('public/assets'));
+var port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({
+    type: 'application/vnd.api+json'
+}));
+
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+var ORM = require('./config/orm');
+// ORM.selectAll();
+// ORM.insertOne('wednesdayBurger_theSecond', false);
+// ORM.updateOne(12);
+
+
+
+
+//Serve static content for the app from the "public" directory in the application directory.
+
+
+app.get('/', function(req, res) {
+
+    myConnection.query('SELECT * FROM burgers', function(err, data) {
+        if (err) throw error
+        res.render('index', {
+            burgers: data
+        })
+    })
+});
+
+
+
+
+app.listen(port, function() {
+    console.log("app is listening");
+})
